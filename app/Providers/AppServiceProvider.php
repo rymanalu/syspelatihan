a6cb35fbb485247c37e17d11be9f18acc58083fa
Roni Yusuf Manalu <rymanalu@gmail.com>
2017-07-04 10:50:25 +0700

    Fix specified key was too long error.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use DB;
+use Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
                 ]);
             });
         }
+
+        Schema::defaultStringLength(191);
     }
 
     /**
